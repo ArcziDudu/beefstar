32393f06bf2bf65ff6115d0e6b1f0078a12bce35
arcziDudu <artur.augustyn26@gmail.com>
2023-11-07 22:54:21 +0100

    Initial commit

---
+++ src/test/java/com/beefstar/beefstar/BeefstarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beefstar.beefstar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeefstarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
